2f610257cbd0de5f8a28e8a7081110d1af4c0d24
YRollHid <yans.roll@gmail.com>
2023-11-14 14:21:22 +0100

    Add EmailAPI Project

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private string connectionString = "Endpoint=sb://mango-yrhpwc.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=3y4rKDOwLQXchWJa9Pi7jFOjcCe3B7WvR+ASbO1NkaU=";
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
+                .UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+    }
+}
--- Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Mango.MessageBus;
 using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
@@ -17,14 +18,18 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private readonly AppDbContext _db;
         private IProductService _productService;
         private ICouponService _couponService;
+        private IMessageBus _messageBus;
+        private IConfiguration _configuration;
         private ResponseDto _response;
         private IMapper _mapper;
 
-        public CartAPIController(AppDbContext db, IMapper mapper, IProductService productService, ICouponService couponService)
+        public CartAPIController(AppDbContext db, IMapper mapper, IProductService productService, ICouponService couponService, IMessageBus messageBus, IConfiguration configuration)
         {
             _db = db;
             _productService = productService;
             _couponService = couponService;
+            _messageBus = messageBus;
+            _configuration = configuration;
             _mapper = mapper;
             _response = new ResponseDto();
             
@@ -86,6 +91,22 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             {
             _response.IsSuccess = false;
             _response.Message = ex.ToString();
+            }
+            return _response;
+        }
+
+        [HttpPost("EmailCartRequest")]
+        public async Task<object> EmailCartRequest([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                await _messageBus.PublishMessage(cartDto, _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue"));
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();
             }
             return _response;
         }
