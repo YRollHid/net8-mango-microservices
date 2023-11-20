bdbea46ae18aa088856dea1608e0eac651e97b4d
YRollHid <yans.roll@gmail.com>
2023-11-20 17:07:26 +0100

    Add consumer for UserEmailRegister in EmailAPI

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -10,19 +10,23 @@ namespace Mango.Services.EmailAPI.Messaging
     {
         private readonly string serviceBusConnectionString;
         private readonly string emailCartQueue;
+        private readonly string registerUserQueue;
         private readonly IConfiguration _configuration;
         private readonly EmailService _emailService;
 
         private ServiceBusProcessor _emailCartProcessor;
+        private ServiceBusProcessor _registerUserProcessor;
         public AzureServiceBusConsumer(IConfiguration configuration, EmailService emailService)
         {
             _emailService = emailService;
             _configuration = configuration;
             serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
             emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+            registerUserQueue = _configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue");
 
             var client = new ServiceBusClient(serviceBusConnectionString);
             _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+            _registerUserProcessor = client.CreateProcessor(registerUserQueue);
         }
 
         public async Task Start()
@@ -30,6 +34,10 @@ namespace Mango.Services.EmailAPI.Messaging
             _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
             _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
             await _emailCartProcessor.StartProcessingAsync();
+
+            _registerUserProcessor.ProcessMessageAsync += OnUserRegisterRequestReceived;
+            _registerUserProcessor.ProcessErrorAsync += ErrorHandler;
+            await _registerUserProcessor.StartProcessingAsync();
         }
 
         private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
@@ -52,6 +60,25 @@ namespace Mango.Services.EmailAPI.Messaging
 
         }
 
+        private async Task OnUserRegisterRequestReceived(ProcessMessageEventArgs args)
+        {
+            //this is where you will receive message
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            string email = JsonConvert.DeserializeObject<string>(body);
+            try
+            {
+                //TODO - try to log email
+                await _emailService.RegisterUserEmailAndLog(email);
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
         private Task ErrorHandler(ProcessErrorEventArgs args)
         {
             Console.WriteLine(args.Exception.ToString());
@@ -62,6 +89,9 @@ namespace Mango.Services.EmailAPI.Messaging
         {
             await _emailCartProcessor.StopProcessingAsync();
             await _emailCartProcessor.DisposeAsync();
+
+            await _registerUserProcessor.StopProcessingAsync();
+            await _registerUserProcessor.DisposeAsync();
         }
     }
 }
