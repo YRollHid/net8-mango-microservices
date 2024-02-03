1458d0797a25660cb126b9aca828552031ecb2c6
YRollHid <yans.roll@gmail.com>
2024-02-03 09:04:04 +0100

    Modify Base Service for Form File

--- Mango.Web/Service/ProductService.cs
+++ Mango.Web/Service/ProductService.cs
@@ -18,8 +18,8 @@ namespace Mango.Web.Service
             {
                 ApiType = SD.ApiType.POST,
                 Data = productDto,
-                Url = SD.ProductAPIBase + "/api/product"
-                //ContentType = SD.ContentType.MultipartFormData
+                Url = SD.ProductAPIBase + "/api/product",
+                ContentType = SD.ContentType.MultipartFormData
             });
         }
 
@@ -56,8 +56,8 @@ namespace Mango.Web.Service
             {
                 ApiType = SD.ApiType.PUT,
                 Data = productDto,
-                Url = SD.ProductAPIBase + "/api/product"
-                //ContentType = SD.ContentType.MultipartFormData
+                Url = SD.ProductAPIBase + "/api/product",
+                ContentType = SD.ContentType.MultipartFormData
             });
         }
     }
